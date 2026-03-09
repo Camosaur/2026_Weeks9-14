9d2bb456caa8218ddc0e4f87a92117787b7bdacf
pletschC <pletschC@OA-S141-01>
2026-03-09 11:08:24 -0400

    Learned how to set up inputs using unity events

---
+++ Assets/Scripts/Week9_Controllers/ControllerInputForMove.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UIElements;
+
+public class ControllerInputForMove : MonoBehaviour
+{
+    public float speed = 5;
+    public Vector2 movement;
+    public Vector2 position;
+    public AudioSource SFX;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        SFX = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += (Vector3)movement * speed * Time.deltaTime;
+        //transform.position = position;
+    }
+
+    public void OnPoint(InputAction.CallbackContext context) { 
+        movement = Camera.main.ScreenToWorldPoint(context.ReadValue<Vector2>());
+    }
+    public void OnMove(InputAction.CallbackContext context) {
+        position = context.ReadValue<Vector2>();
+    }
+
+    public void OnAttack(InputAction.CallbackContext context) {
+        if (context.performed) { 
+            SFX.Play();
+        }
+    }
+}
