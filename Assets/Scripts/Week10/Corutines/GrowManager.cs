5024bb1d8c5246a05b42e93dd0da2d794993e3bd
CAMERON\camer <cameronpletsch@gmail.com>
2026-03-14 14:17:35 -0400

    Coruntines aren't so bad! Their cool. Why couldn't unity include this functionality in every function though? Mabye they would have had to make their own coding laguage like Godot did?

---
+++ Assets/Scripts/Week10/Corutines/GrowManager.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using UnityEngine;
+
+public class GrowManager : MonoBehaviour
+{
+    public Transform rockTransform;
+    public Transform sigilTransform;
+
+    public float sigilDelay = 1;
+    bool isRunning = false; //Is the corutine running? This makes it ok to spam the button, so that there is only ever one instance of the corunine running at a time.
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rockTransform.localScale = Vector2.zero;
+        sigilTransform.localScale = Vector2.zero;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void startRockGrowing() {
+        if (!isRunning) {
+            StartCoroutine(growRock());
+        }
+        
+    }
+
+    IEnumerator growRock()
+    {
+        rockTransform.localScale = Vector2.zero;
+        sigilTransform.localScale = Vector2.zero;
+        float t = 0;
+
+        isRunning = true;
+
+        while (t < 1) { 
+            t += Time.deltaTime;
+            rockTransform.localScale = Vector2.one * t; 
+            yield return null;
+        }
+
+        yield return new WaitForSeconds(sigilDelay);
+
+        t = 0;
+        while (t < 1)
+        {
+            t += Time.deltaTime;
+            sigilTransform.localScale = Vector2.one * t;
+            yield return null;
+        }
+
+        isRunning =false;
+    }
+}
