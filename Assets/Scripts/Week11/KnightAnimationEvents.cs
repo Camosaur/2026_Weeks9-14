d25c63b20d2c2349563e499422fa63f4fac25558
pletschC <pletschC@OA-S141-01>
2026-03-23 10:55:00 -0400

    Made a knight that moves to the mouse on click.

--- Assets/Scripts/Week11/KnightAnimationEvents.cs
+++ Assets/Scripts/Week11/KnightAnimationEvents.cs
@@ -1,12 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class KnightAnimationEvents : MonoBehaviour
 {
     public AudioSource SFX;
+    public float speed;
+
+    Coroutine vortexMovement;
+
+    public Vector2 mousePos;
+
+    public Animator animator;
+
+    public List<AudioClip> stepSounds;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        //StartCoroutine(moveToPoint(new Vector2(0, 0)));
     }
 
     // Update is called once per frame
@@ -15,8 +28,57 @@ public class KnightAnimationEvents : MonoBehaviour
         
     }
 
+    public void OnPoint(InputAction.CallbackContext context) {
+
+        mousePos = Camera.main.ScreenToWorldPoint(context.ReadValue<Vector2>());
+    }
+
+    public void OnClick(InputAction.CallbackContext context) {
+
+        if (vortexMovement != null) { 
+            StopCoroutine(vortexMovement);
+        }
+
+        vortexMovement = StartCoroutine(moveToPoint(mousePos));
+
+    }
+
     public void Footsteps() {
 
+        SFX.pitch = Random.Range(1, 2);
+        SFX.clip = stepSounds[Random.Range(0, stepSounds.Count)];
         SFX.Play();
+    }
+
+    //Use this in the project.... You'll have to remove the animation stuff, but it's perfect!
+    IEnumerator moveToPoint(Vector2 pointToMoveTo) {
+        animator.SetBool("IsMoving", true);
+        //Find the direction to the spot
+        Vector2 direction = pointToMoveTo - (Vector2)transform.position;
+
+        //flip the rotation correctly
+        if (direction.x < 0)
+        {
+            GetComponent<SpriteRenderer>().flipX = true;
+        }
+        else {
+            GetComponent<SpriteRenderer>().flipX = false;
+        }
+
+            //So that it goes the intended speed
+            direction = Vector2.Normalize(direction);
+
+        //While it isn't at that spot
+        while (Vector2.Distance(transform.position, pointToMoveTo) > 0.5f) {
+
+            //Move toward that spot
+            transform.position += (Vector3)direction * Time.deltaTime * speed;
+
+            //Wait for the next frame
+            yield return null;
+        }
+
+        animator.SetBool("IsMoving", false);
+
     }
 }
