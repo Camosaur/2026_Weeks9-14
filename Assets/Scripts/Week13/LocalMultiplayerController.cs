2b898706c77639411f4e122219c02005af99ecef
pletschC <pletschC@OA-S141-01>
2026-04-06 09:32:14 -0400

    Multiplayer works!

--- Assets/Scripts/Week13/LocalMultiplayerController.cs
+++ Assets/Scripts/Week13/LocalMultiplayerController.cs
@@ -5,7 +5,14 @@ public class LocalMultiplayerController : MonoBehaviour
 {
     public Vector2 moveInput;
     public float speed = 5;
+    PlayerInput input;
 
+    private void Start()
+    {
+        input = GetComponent<PlayerInput>();
+
+        GetComponent<SpriteRenderer>().color = Random.ColorHSV();
+    }
     // Update is called once per frame
     void Update()
     {
@@ -20,7 +27,7 @@ public class LocalMultiplayerController : MonoBehaviour
 
     public void OnAttack(InputAction.CallbackContext context) {
 
-        if (context.performed) { Debug.Log("Attack!"); }
+        if (context.performed) { Debug.Log("Player "+(input.playerIndex+1)+": Attacks!"); }
 
     }
 }
